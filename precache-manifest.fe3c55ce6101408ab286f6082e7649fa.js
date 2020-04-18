2708c5a45780fc5f863b3c807a1a9a2836956102
Adi <adityaraj.chauhan@scanbuy.com>
2020-04-18 00:22:40 -0400

    Updates

---
+++ precache-manifest.fe3c55ce6101408ab286f6082e7649fa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20e065649f86d060c35692f4409d7cef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edc39de34f618635dab4",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6b49b87cc2155337cad0",
+    "url": "/static/js/2.cdd5f70c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.cdd5f70c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edc39de34f618635dab4",
+    "url": "/static/js/main.1a06da18.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  },
+  {
+    "revision": "d0aad2b1a2e5a78242cb2d01e5098e8d",
+    "url": "/static/media/blog-post.1.d0aad2b1.md"
+  },
+  {
+    "revision": "d0aad2b1a2e5a78242cb2d01e5098e8d",
+    "url": "/static/media/blog-post.2.d0aad2b1.md"
+  },
+  {
+    "revision": "d0aad2b1a2e5a78242cb2d01e5098e8d",
+    "url": "/static/media/blog-post.3.d0aad2b1.md"
+  }
+]);
